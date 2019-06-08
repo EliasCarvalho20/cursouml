d8c62fc6fcd97867a1205e068037119343bd4d9c
Elias Carvalho <eliascarvalho01@hotmail.com>
2019-06-08 13:14:15 -0300

    Criando operacao de instanciacao

--- src/main/java/com/curso/uml/CursoumlApplication.java
+++ src/main/java/com/curso/uml/CursoumlApplication.java
@@ -1,13 +1,34 @@
 package com.curso.uml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoumlApplication {
+import com.curso.uml.domain.Categoria;
+import com.curso.uml.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoumlApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+	}
+	
+	
 
 }
